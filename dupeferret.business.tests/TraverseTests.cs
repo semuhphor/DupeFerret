41fb57995a89ff3284c45dda547cbd237d235970
bob flanders <bob.flanders@gmail.com>
2018-07-23 21:23:39 -0400

    BF - Built code to find the TestData directory. Is there a better way...

--- dupeferret.business.tests/TraverseTests.cs
+++ dupeferret.business.tests/TraverseTests.cs
@@ -8,18 +8,30 @@ namespace dupeferret.business.tests
     public class TraverseTests
     {
         public readonly Traverse _traverse;
-        public readonly string _testDataDirectory = Directory.GetCurrentDirectory();
 
-        public TraverseTests()
+        public TraverseTests() => _traverse = new Traverse();
+
+        private string TrimOneDirectory(string path)
         {
-            _traverse = new Traverse();
-            
+            return Path.GetFullPath(path + Path.DirectorySeparatorChar + "..");
+        }
+        private string TestDataDirectory
+        {
+            get
+            {
+                var codeBaseUrl = new Uri(typeof(TraverseTests).Assembly.CodeBase);
+                var codeBasePath = Uri.UnescapeDataString(codeBaseUrl.AbsolutePath); 
+                var path = Path.GetDirectoryName(codeBasePath);
+                while (!path.EndsWith(Path.DirectorySeparatorChar + "bin"))
+                {
+                    path = TrimOneDirectory(path);
+                }
+                path = TrimOneDirectory(path) + Path.DirectorySeparatorChar + "TestData";
+                return path;
+            }
         }
 
         [Fact]
-        public void SetCurrentDirectory()
-        {
-            Console.WriteLine(_testDataDirectory);
-        }
+        void SetCurrentDirectory() => Console.WriteLine(TestDataDirectory);
     }
 }
